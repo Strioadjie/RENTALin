8c9aa6a0530bd5b7c8434ef38b78d4f2998917ae
Strioadjie <satrioadjie117@gmail.com>
2025-12-09 21:59:02 +0700

    Commit pertama (upload file2 yang lama)

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Swiper initialization for the Trending Games carousel (minimal, kept compact)
+const trendingSwiper = new Swiper('.trending-swiper', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    }
+});
